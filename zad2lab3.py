299cf96d3d2dbbfa305d96a3f1f242d33335be1c
xxMateusz <mateusz.bochenek@microsoft.wsei.edu.pl>
2024-03-25 20:43:31 +0100

    g

---
+++ zad2lab3.py
@@ -0,0 +1,22 @@
+
+figura = ["prostokat","trapez","kolo"]
+print(f"wybierz jedna z figur{figura}")
+
+figuraUzytkownika = input()
+print(figuraUzytkownika)
+while figuraUzytkownika != figura:
+     
+     if figuraUzytkownika is figura:
+      break
+     else :
+      input(f"Podaj poprawna figure {figuraUzytkownika}")
+
+dane = input("podaj wymiary figury: bok wysokosc i promien jesli jest wymagana")
+wymiary = dane.split(' ')
+if figuraUzytkownika in figura:
+   if figuraUzytkownika == figura[0]:
+    print(f"pole prostokata wynosi {int(wymiary[0]) * int(wymiary[1])}")
+   elif figuraUzytkownika == figura[1]:
+    print(f"pole trapezu wynosi {(int(wymiary[0]) + int(wymiary[1]))* int(wymiary[2]) * 1/2}")
+   elif figuraUzytkownika == figura[2]:
+    print(f" pole kola wynosi {int(wymiary[2]) *int(wymiary[2]) * 3.14 }")
